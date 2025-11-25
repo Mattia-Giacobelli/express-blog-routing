bb819eb9c6a37db17bf50491b6bdd93b39645f69
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-25 13:01:19 +0100

    Create server responses

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,39 @@
 const express = require('express')
+
+//Import posts array
+const posts = require('../posts')
+
 // create posts router
 const router = express.Router()
 
 //Index 
-router.get()
+router.get('/', (req, res) => {
+    res.send('Posts list')
+})
 
+//Show 
+router.get('/:id', (req, res) => {
+    res.send(`Post with id:${req.params.id}`)
+})
 
+//Store
+router.post('/', (req, res) => {
+    res.send('Add new posts')
+})
+
+//Update
+router.put('/:id', (req, res) => {
+    res.send(`Update post with id:${req.params.id}`)
+})
+
+//Modify 
+router.patch('/:id', (req, res) => {
+    res.send(`Modify post with id:${req.params.id}`)
+})
+
+//Destroy
+router.delete('/:id', (req, res) => {
+    res.send(`Delete post with id:${req.params.id}`)
+})
 
 module.exports = router
